e6a66f68b88ff4eeb2a01b762ac8ad0fac38208e
unknown <apple895263@gmail.com>
2022-10-23 16:03:13 +0800

    更新 web-ultis、Jinja2 base 模板

--- Flask_web/YOLOtalk.py
+++ Flask_web/YOLOtalk.py
@@ -8,6 +8,7 @@ import json
 import os
 import jinja2
 
+from web_ultis import *
 # import below is jim's YOLOtalk code
 import sys
 sys.path.append("..") 
@@ -18,149 +19,69 @@ from libs.utils import *
 
 app = Flask(__name__)
 
-oldFences = os.listdir(r'static/alias_pict')
-FencesNames = []
-for name in oldFences :
-    if "ipynb" in name:
-        continue
-    name = name[:-4]
-    FencesNames.append(name)
 
 
-# Temporary information
-data  = {"alias":"",
-         "viedo_url":"",
-         "add_time":"",
-         "fence": {}
-        }
-
-def on_data(img_path, group, alias, results): 
-
-    for det in results:
-        class_name = det[0]
-        confidence = det[1]
-        center_x, center_y, width, height = det[2]
-        left, top, right, bottom = darknet.bbox2points(det[2])
-#             print(class_name, confidence, center_x, center_y)
-#         if len(results) > 0:            
-#             LineNotify.line_notify(class_name)   # LINENOTIFY.py  token
-#             DAN.push('yPerson-I', str(class_name), center_x, center_y, img_path)
+# Restart YoloDevice
 
 
-def transform_vertex(old_vertex):
-    # transform vertex from [x1,y1,x2,y2,x3,....]  to [(x1,y1),(x2,y2),...]
-    new_vertex = []
-    a = old_vertex.split(",") 
+def Restart_YoloDevice():
+    oldJson = os.listdir(r"static/Json_Info")
+    actived_yolo = {}
+    for j in oldJson :
+        if "ipynb" in j:
+            continue
 
-    for i in range(0,len(a),2):
-        x = (int(a[i]), int(a[i+1]))
-        new_vertex.append(x)    
+        path = os.path.join("static/Json_Info", j)
+        with open(path, 'r', encoding='utf-8') as f:             
+            Jdata = json.load(f)
 
-    return new_vertex
+        alias = Jdata["alias"]
+        URL   = Jdata["viedo_url"]
 
+        if Jdata["fence"]  == {}:
+            vertex = None
 
-# active YOLO already in Json file
-oldJson = os.listdir(r"static/Json_Info")
-actived_yolo = {}
-for j in oldJson :
-    if "ipynb" in j:
-        continue
+        else:
+            key_list = list(Jdata["fence"].keys())
+            vertex = {}
 
-    path = os.path.join("static/Json_Info", j)
-    with open(path, 'r', encoding='utf-8') as f:             
-        Jdata = json.load(f)
+            for key in key_list :
+                old_vertex = Jdata["fence"][key]["vertex"][1:-1]
+                # vertex
+                new_vertex = transform_vertex(old_vertex)
+                vertex[key] = new_vertex 
+                # sensitivity
+                old_sensitivity = float(Jdata["fence"][key]["Sensitivity"])
 
- 
-    alias = Jdata["alias"]
-    URL   = Jdata["viedo_url"]
-
-    if Jdata["fence"]  == {}:
-        print("fence is none")
-        vertex = None
-    else:
-        key_list = list(Jdata["fence"].keys())
-        vertex = {}
-        for key in key_list :
-            old_vertex = Jdata["fence"][key]["vertex"][1:-1]
-
-            new_vertex = transform_vertex(old_vertex)
-            vertex[key] = new_vertex 
-
-        print("===========================")
-        print(f" Alias name is :  {alias}")
-        print(f" Fence name is : {key_list}")
-        print(f" Json is : {j}")
-        print(f" Vertex is :{vertex}")
-
-# ======== FOR YOLO ========
-        yolo1 = YoloDevice(
-                        config_file = '../darknet/cfg/yolov4-tiny.cfg',
-                        data_file = '../darknet/cfg/coco.data',
-                        weights_file = '../weights/yolov4-tiny.weights',
-                        thresh = 0.3,                 # need modify (ok)
-                        output_dir = './static/record/',              
-                        video_url = URL,              # need modify (ok) 
-                        is_threading = True,          # rtsp  ->true  video->false (ok)
-                        vertex = vertex,              # need modify (ok)    
-                        alias = alias,                # need modify (ok)
-                        display_message = True,
-                        obj_trace = True,        
-                        save_img = False,
-                        save_video = False,           # modify to False
-                        target_classes = ["person"],
-                        auto_restart = False,
-                        )    
-        yolo1.set_listener(on_data)
-        yolo1.start()
-        actived_yolo[alias] = yolo1
-# ======== FOR YOLO ========
-
-def gen_frames(yolo):
-
-    filepath = "static/Json_Info/camera_info_" + str(yolo.alias) + ".json"
-    with open(filepath, 'r', encoding='utf-8') as f:                    
-        Jdata = json.load(f)
-
-    key_list = list(Jdata["fence"].keys())
-    vertex = {}
-
-    while True:
-        frame = yolo.get_current_frame()
-        # copy_frame = frame
-
-        for key in key_list :
-
-            old_vertex = Jdata["fence"][key]["vertex"][1:-1]
-            new_vertex = transform_vertex(old_vertex)
-            vertex[key] = new_vertex
-
-        frame = draw_polylines(frame, vertex)  # draw the polygon
-            
-        if len(yolo.detect_target) != 0:
-            mask = np.zeros((frame.shape), dtype = np.uint8)
-            pts = []
-
-            
-            for singal_vertex in vertex.values():
-                temp =[]
-                for point in singal_vertex :
-                    temp.append(point)
-                pts.append(np.array(temp, dtype=np.int32))
-
-            mask = cv2.fillPoly(mask, pts, (180,0,255))  # Filling the mask of polygon 
-            frame =  0.5 * mask + frame  
-
-            
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                yolo1 = YoloDevice(
+                                config_file = '../darknet/cfg/yolov4-tiny.cfg',
+                                data_file = '../darknet/cfg/coco.data',
+                                weights_file = '../weights/yolov4-tiny.weights',
+                                thresh = old_sensitivity,                 
+                                output_dir = './static/record/',              
+                                video_url = URL,              
+                                is_threading = True,          
+                                vertex = new_vertex,                 
+                                alias = alias,                
+                                display_message = True,
+                                obj_trace = True,        
+                                save_img = False,
+                                save_video = False,           
+                                target_classes = ["person"],
+                                auto_restart = False,
+                                )    
+                yolo1.set_listener(on_data)
+                yolo1.start()
+                actived_yolo[alias] = yolo1
+        # ======== FOR YOLO ========
+    return actived_yolo
+actived_yolo = Restart_YoloDevice()
 
 @app.route('/',methods=[ 'GET','POST'])
 def home():
-    print(f"[actived_yolo] :{actived_yolo} ")
+    
+    all_fences_names = read_all_fences()
+
     if request.method == 'POST':
 
         alias  = request.form.get('area')
@@ -169,31 +90,20 @@ def home():
 
         
         if URL =="REPLOT":
-            print("REPLOT")
-            IMGpath = "static/alias_pict/"+str(alias)+".jpg"
-            data["alias"]=alias
-            data["viedo_url"]=URL
-            data["add_time"]=Addtime
-   
-            filepath = "static/Json_Info/camera_info_" + str(alias) + ".json"
-            with open(filepath, 'r', encoding='utf-8') as f:                    
-                Jdata = json.load(f)
-            old_fence_data = Jdata['fence']
+            IMGpath, shape =  replot(alias, URL, Addtime)
+            return render_template('plotarea.html', data=IMGpath, name=str(alias), shape=shape)
 
-            fences = list(old_fence_data.keys())
-
-            vertexs = []
-
-            for fence in fences:
-                old_vertex = old_fence_data[fence]['vertex']
-                vertexs.append(old_vertex)  
-
-            return render_template('plotarea.html', data = IMGpath, name=str(alias))
-        
         else:    
 
             fig = cv2.VideoCapture(str(URL))
             stat, I = fig.read()
+
+            # Temporary information
+            data  = {"alias":"",
+                    "viedo_url":"",
+                    "add_time":"",
+                    "fence": {}
+                    }
 
             data["alias"]     = alias
             data["viedo_url"] = URL
@@ -201,15 +111,15 @@ def home():
             
 
             filepath = "static/Json_Info/camera_info_" + data["alias"] + ".json"
-            with open(filepath, 'w', encoding='utf-8') as file:             # 
+            with open(filepath, 'w', encoding='utf-8') as file:             
                 json.dump(data, file,separators=(',\n', ':'),indent = 4)
         
         
             IMGpath = "static/alias_pict/"+str(alias)+".jpg"
-            FencesNames.append(str(alias))
+            all_fences_names.append(str(alias))
             cv2.imwrite(IMGpath,I)
 
-# ======== FOR YOLO ========
+    # ======== FOR YOLO ========
             yolo1 = YoloDevice(
                     config_file = '../darknet/cfg/yolov4-tiny.cfg',
                     data_file = '../darknet/cfg/coco.data',
@@ -230,16 +140,19 @@ def home():
             yolo1.set_listener(on_data)
             yolo1.start()
             actived_yolo[alias] = yolo1
-# ======== FOR YOLO ========
+    # ======== FOR YOLO ========
         
-            return render_template('plotarea.html', data = IMGpath, name=str(alias))
+            return render_template('plotarea.html', data = IMGpath, name=str(alias), shape = I.shape)
      
-    return render_template('home.html', navs = FencesNames)
+    return render_template('home.html', navs = all_fences_names)
     
 
 
 @app.route('/plotarea',methods=[ 'GET','POST'])
 def plotarea():
+
+    all_fences_names = read_all_fences()
+
     if request.method == 'POST':
         
         print("plotarea enter & POST")
@@ -247,6 +160,8 @@ def plotarea():
         alias = request.form['alias']
         FenceName = request.form['FenceName']   # plot name
         vertex = request.form['vertex']         # plot point
+
+        IMGpath, shape =  replot(alias, URL, Addtime)
 
         Fence_info={'vertex':vertex,
                     'Group':'-',                
@@ -288,43 +203,27 @@ def plotarea():
             data['fence']={}                                                
 
 
-    return render_template('plotarea.html')
+    return render_template('plotarea.html', data=IMGpath, name=str(alias), shape=shape)
 
 
 
 @app.route('/management',methods=[ 'GET','POST'])
 def management():
+
+    all_fences_names = read_all_fences()
     # nav Replot 
     if request.method == 'POST' :
-
 
         alias   = request.form.get('area')
         URL     = request.form.get('URL')        
         Addtime = str(time.asctime( time.localtime(time.time()) ))[4:-5]
         
-        if (URL == "REPLOT"):
-
-            IMGpath = "static/alias_pict/"+str(alias)+".jpg"
-            data["alias"]     = alias
-            data["viedo_url"] = URL
-            data["add_time"]  = Addtime
-                
-            filepath = "static/Json_Info/camera_info_" + str(alias) + ".json"
-            with open(filepath, 'r', encoding='utf-8') as f:                    
-                Jdata = json.load(f)
-            old_fence_data = Jdata['fence']
-
-            fences = list(old_fence_data.keys())
-
-            vertexs = []
-
-            for fence in fences:
-                old_vertex = old_fence_data[fence]['vertex']
-                vertexs.append(old_vertex)  
-
-            return render_template('plotarea.html', data = IMGpath, name=str(alias))
-
+        IMGpath, shape =  replot(alias, URL, Addtime)
         
+        if URL =="REPLOT":
+            
+            return render_template('plotarea.html', data=IMGpath, name=str(alias), shape=shape)
+
         if (URL == "Edit"):
             
             alias       = request.form.get('alias')
@@ -341,12 +240,18 @@ def management():
             Jdata['fence'][FenceName]['Group']       = Group
             Jdata['fence'][FenceName]['Alarm_Level'] = Alarm_Level
             Jdata['fence'][FenceName]['Note']        = Note
+
+            old_Sensitivity = Jdata['fence'][FenceName]['Sensitivity']
             Jdata['fence'][FenceName]['Sensitivity'] = Sensitivity
+
+            if old_Sensitivity != Sensitivity :
+                print(f"actived_yolo[alias].thresh : {actived_yolo[alias].thresh}")
+                actived_yolo[alias].thresh = float(Sensitivity)
 
             with open(filepath, 'w', encoding='utf-8') as file:                 
                 json.dump(Jdata, file,separators=(',\n', ':'),indent = 4)
 
-            return render_template('management.html', navs=FencesNames)
+            return render_template('management.html', navs=all_fences_names)
 
         if (URL == "Delete"):    
             print("Enter Delete")
@@ -378,77 +283,46 @@ def management():
             with open(path, 'r', encoding='utf-8') as f:
                 file = json.load(f)
                 items.append(file)
-    return render_template('management.html', navs=FencesNames, items=items)
-
+    return render_template('management.html', navs=all_fences_names, items=items)
 
 
 @app.route('/streaming',methods=[ 'GET','POST'])
 def streaming():
+
+    all_fences_names = read_all_fences()
+
     if request.method == 'POST' :
 
         alias   = request.form.get('area')
         URL     = request.form.get('URL')        
         Addtime = str(time.asctime( time.localtime(time.time()) ))[4:-5]
         
-        if (URL == "REPLOT"):
+        if URL =="REPLOT":
 
-            IMGpath = "static/alias_pict/"+str(alias)+".jpg"
-            data["alias"]     = alias
-            data["viedo_url"] = URL
-            data["add_time"]  = Addtime
-                
-            filepath = "static/Json_Info/camera_info_" + str(alias) + ".json"
-            with open(filepath, 'r', encoding='utf-8') as f:                     
-                Jdata = json.load(f)
-            old_fence_data = Jdata['fence']
-
-            fences = list(old_fence_data.keys())
-
-            vertexs = []
-
-            for fence in fences:
-                old_vertex = old_fence_data[fence]['vertex']
-                vertexs.append(old_vertex)  
-
-            return render_template('plotarea.html', data = IMGpath, name=str(alias))
+            IMGpath, shape =  replot(alias, URL, Addtime)
+            return render_template('plotarea.html', data=IMGpath, name=str(alias), shape=shape)
 
     alias_list = os.listdir(r'static/alias_pict')
     alias_list.remove('.ipynb_checkpoints')
 
-    return render_template('streaming.html', navs=FencesNames, alias_list=alias_list, length=len(alias_list))
-
+    return render_template('streaming.html', navs=all_fences_names, alias_list=alias_list, length=len(alias_list))
 
 
 @app.route('/schedule',methods=[ 'GET','POST'])
 def schedule():
     
+    all_fences_names = read_all_fences()
+
     if request.method == 'POST' :
 
         alias   = request.form.get('area')
         URL     = request.form.get('URL')        
         Addtime = str(time.asctime( time.localtime(time.time()) ))[4:-5]
             
-        if (URL == "REPLOT"):
+        if URL =="REPLOT":
 
-            IMGpath = "static/alias_pict/"+str(alias)+".jpg"
-            data["alias"]     = alias
-            data["viedo_url"] = URL
-            data["add_time"]  = Addtime
-  
-            filepath = "static/Json_Info/camera_info_" + str(alias) + ".json"
-            with open(filepath, 'r', encoding='utf-8') as f:                    
-                Jdata = json.load(f)
-            old_fence_data = Jdata['fence']
-
-            fences = list(old_fence_data.keys())
-
-            vertexs = []
-
-            for fence in fences:
-                old_vertex = old_fence_data[fence]['vertex']
-                vertexs.append(old_vertex)  
-
-            return render_template('plotarea.html', data = IMGpath, name=str(alias))
+            IMGpath, shape =  replot(alias, URL, Addtime)
+            return render_template('plotarea.html', data=IMGpath, name=str(alias), shape=shape)
 
         
         if (URL == "Edit_time"):
@@ -472,13 +346,10 @@ def schedule():
                 # Jdata['fence'][FenceName]['Group'] = Group
                 Jdata['fence'][FenceName]['Schedule'][Order]['Start_time']  = Start_time
                 Jdata['fence'][FenceName]['Schedule'][Order]['End_time']    = End_time
-                print("in")
+
             else:
                 # data['fence'][FenceName]['Group'] = Group
-
                 Jdata['fence'][FenceName]['Schedule'][Order] = new_schedule
-                print("not in")
-                print(Jdata['fence'][FenceName]['Schedule'][Order])
 
             with open(filepath, 'w', encoding='utf-8') as file:                  
                 json.dump(Jdata, file,separators=(',\n', ':'),indent = 4)
@@ -511,7 +382,8 @@ def schedule():
                 file = json.load(f)
                 items.append(file)
   
-    return render_template('schedule.html', navs=FencesNames, items=items)
+    return render_template('schedule.html', navs=all_fences_names, items=items)
+
 
 @app.route('/video/<order>',methods=[ 'GET','POST'])
 def video_feed(order):
@@ -526,11 +398,14 @@ def video_feed(order):
         return 'Error'
 
 
-# @app.route('/test',methods=[ 'GET','POST'])
-# def test():
+@app.route('/test',methods=[ 'GET','POST'])
+def test():
     
-#     return render_template('test.html', )
+    return render_template('base.html', )
 
-  
+@app.route('/base2',methods=[ 'GET','POST'])
+def base2():
+    
+    return render_template('test_base.html', )  
 if __name__ == '__main__':
     app.run(debug = True, host="0.0.0.0",port="10328")
