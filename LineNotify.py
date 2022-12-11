369893111e20f06d1a00e5c74c596d503b74e12c
unknown <apple895263@gmail.com>
2022-12-12 04:22:26 +0800

    schedule & Files & postURL

---
+++ LineNotify.py
@@ -0,0 +1,10 @@
+import lineTool
+import requests
+
+# Change 'token_key' to your Line token
+def line_notify(msg):
+    token_key = ''  
+    header = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":'Bearer '+token_key}
+    URL = 'https://notify-api.line.me/api/notify'
+    payload = {'message':msg}
+    res=requests.post(URL,headers=header,data=payload)
